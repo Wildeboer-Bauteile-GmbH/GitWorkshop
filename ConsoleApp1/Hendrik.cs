ab954017f01ed3b2d5832c005352d7a50c340de4
Jörg Boekhoff <joerg.boekhoff@wildeboer.de>
2025-02-12 06:25:28 +0100

    Konstruktoren korrigiert; compilierbar gemacht

--- ConsoleApp1/Hendrik.cs
+++ ConsoleApp1/Hendrik.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1;
+﻿namespace ConsoleApp1;
 
 internal class Hendrik
 {
     // Test
     public Hendrik()
     {
-      // Test
-        public Hendrik()
-        {
-            // Test TEST
-            int a = 0;  
-            int b = 1;
-        }
+        // Test TEST
+        int a = 0;
+        int b = 1;
     }
 }
